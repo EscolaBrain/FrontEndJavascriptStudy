7d7742ee8f2c8a241a129f7e0cc63c3ae90c1d6f
Escolahead <paulo.henriquenunes@hotmail.com>
2018-10-07 15:13:28 -0300

    po

---
+++ EstruturasDeControle/If1.js
@@ -0,0 +1,13 @@
+function soBoaNoticia(nota){
+    if(nota >= 7){
+        console.log("aluno passou " +nota);
+    }
+}
+soBoaNoticia(8.1)
+soBoaNoticia(5.4)
+
+function seForVerdadeEuFalo(valor){
+    if(valor){
+        console.log('É verdade... ' + valor)
+    }
+}
